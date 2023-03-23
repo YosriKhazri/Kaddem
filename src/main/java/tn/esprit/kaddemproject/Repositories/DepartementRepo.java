5531d9e8138ab5bb19deed47a8dec238b60f3eaa
YosriKhazri <94873231+YosriKhazri@users.noreply.github.com>
2023-03-23 23:56:51 +0100

    Revert "ichrakRepo"

--- src/main/java/tn/esprit/kaddemproject/Repositories/DepartementRepo.java
+++
@@ -1,9 +0,0 @@
-package tn.esprit.kaddemproject.Repositories;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-import tn.esprit.kaddemproject.Entites.Departement;
-@Repository
-public interface DepartementRepo extends CrudRepository<Departement,Long>{
-
-}
