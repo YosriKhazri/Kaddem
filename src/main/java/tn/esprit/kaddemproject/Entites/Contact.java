41168efd0ffc2d62240ce8b454418a18b2f9b7b6
ykhazri <tkhazri@vermeg.com>
2023-04-13 10:13:41 +0100

    conflit merge

--- src/main/java/tn/esprit/kaddemproject/Entites/Contact.java
+++ src/main/java/tn/esprit/kaddemproject/Entites/Contact.java
@@ -1,4 +1,8 @@
 package tn.esprit.kaddemproject.Entites;
 
 public class Contact {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "idcontrat")
+    long idContratRepo;
 }
